9356517fb731a3388e3e44598cd6538d00e688ba
Maximilian Seidler <maximilian.seidler@soundwork.at>
2025-10-18 10:16:21 +0200

    also for base 16 colors provided
    I am not using them. I still have no clue how terminal colors work in
    neovim.

--- lua/penumbra/terminal.lua
+++ lua/penumbra/terminal.lua
@@ -6,7 +6,7 @@ function M.setup()
 	if not cfg.term_colors then
 		return
 	end
-	vim.g.terminal_color_0 = c.bg
+	vim.g.terminal_color_0 = c.bg_p
 	vim.g.terminal_color_1 = c.red
 	vim.g.terminal_color_2 = c.green
 	vim.g.terminal_color_3 = c.yellow
