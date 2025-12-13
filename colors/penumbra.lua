5688da904e0fb414b785091ee71a53d3b4059f1f
jbisits <jbisits@gmail.com>
2025-12-13 14:05:34 +1100

    Code format on save

--- colors/penumbra.lua
+++ colors/penumbra.lua
@@ -1,6 +1,8 @@
 for k in pairs(package.loaded) do
-    if k:match(".*penumbra.*") then package.loaded[k] = nil end
+	if k:match(".*penumbra.*") then
+		package.loaded[k] = nil
+	end
 end
 
-require('penumbra').setup()
-require('penumbra').colorscheme()
+require("penumbra").setup()
+require("penumbra").colorscheme()
