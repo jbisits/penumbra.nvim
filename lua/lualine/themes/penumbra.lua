386fd9b9ae891fb8942ca914d0bbd479c9293b0f
jbisits <jbisits@gmail.com>
2026-06-01 15:10:00 +1000

    Use lua styl.toml

--- lua/lualine/themes/penumbra.lua
+++ lua/lualine/themes/penumbra.lua
@@ -1,34 +1,34 @@
-local c = require("penumbra.colors")
+local c = require 'penumbra.colors'
 local cfg = vim.g.penumbra_config
 local colors = {
-	bg = c.bg,
-	fg = c.fg,
-	red = c.red,
-	green = c.green,
-	yellow = c.yellow,
-	blue = c.blue,
-	purple = c.purple,
-	cyan = c.cyan,
-	magenta = c.purple_c,
-	grey = c.sky_m,
+  bg = c.bg,
+  fg = c.fg,
+  red = c.red,
+  green = c.green,
+  yellow = c.yellow,
+  blue = c.blue,
+  purple = c.purple,
+  cyan = c.cyan,
+  magenta = c.purple_c,
+  grey = c.sky_m,
 }
 
 local penumbra = {
-	inactive = {
-		a = { fg = colors.grey, bg = colors.bg, gui = "bold" },
-		b = { fg = colors.grey, bg = colors.bg },
-		c = { fg = colors.grey, bg = cfg.transparent and c.none or c.bg_p },
-	},
-	normal = {
-		a = { fg = colors.bg, bg = colors.green, gui = "bold" },
-		b = { fg = colors.magenta, bg = c.bg_m },
-		c = { fg = colors.grey, bg = cfg.lualine.transparent and c.none or c.bg_p },
-		y = { fg = colors.fg, bg = c.bg_m },
-	},
-	visual = { a = { fg = colors.bg, bg = colors.purple, gui = "bold" } },
-	replace = { a = { fg = colors.bg, bg = colors.red, gui = "bold" } },
-	insert = { a = { fg = colors.bg, bg = colors.blue, gui = "bold" } },
-	command = { a = { fg = colors.bg, bg = colors.yellow, gui = "bold" } },
-	terminal = { a = { fg = colors.bg, bg = colors.cyan, gui = "bold" } },
+  inactive = {
+    a = { fg = colors.grey, bg = colors.bg, gui = 'bold' },
+    b = { fg = colors.grey, bg = colors.bg },
+    c = { fg = colors.grey, bg = cfg.transparent and c.none or c.bg_p },
+  },
+  normal = {
+    a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
+    b = { fg = colors.magenta, bg = c.bg_m },
+    c = { fg = colors.grey, bg = cfg.lualine.transparent and c.none or c.bg_p },
+    y = { fg = colors.fg, bg = c.bg_m },
+  },
+  visual = { a = { fg = colors.bg, bg = colors.purple, gui = 'bold' } },
+  replace = { a = { fg = colors.bg, bg = colors.red, gui = 'bold' } },
+  insert = { a = { fg = colors.bg, bg = colors.blue, gui = 'bold' } },
+  command = { a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' } },
+  terminal = { a = { fg = colors.bg, bg = colors.cyan, gui = 'bold' } },
 }
 return penumbra
--- lua/penumbra/colors.lua
+++ lua/penumbra/colors.lua
@@ -1,18 +1,18 @@
-local colors = require("penumbra.palette")
+local colors = require 'penumbra.palette'
 
 local function select_colors()
-	local selected = { none = "none" }
+  local selected = { none = 'none' }
 
-	if vim.g.penumbra_config.style then
-		selected = vim.tbl_extend("force", selected, colors[vim.g.penumbra_config.style])
-	elseif vim.o.background == "light" then
-		selected = vim.tbl_extend("force", selected, colors[vim.g.penumbra_config.flavor .. "_light"])
-	elseif vim.o.background == "dark" then
-		selected = vim.tbl_extend("force", selected, colors[vim.g.penumbra_config.flavor .. "_dark"])
-	end
+  if vim.g.penumbra_config.style then
+    selected = vim.tbl_extend('force', selected, colors[vim.g.penumbra_config.style])
+  elseif vim.o.background == 'light' then
+    selected = vim.tbl_extend('force', selected, colors[vim.g.penumbra_config.flavor .. '_light'])
+  elseif vim.o.background == 'dark' then
+    selected = vim.tbl_extend('force', selected, colors[vim.g.penumbra_config.flavor .. '_dark'])
+  end
 
-	selected = vim.tbl_extend("force", selected, vim.g.penumbra_config.colors)
-	return selected
+  selected = vim.tbl_extend('force', selected, vim.g.penumbra_config.colors)
+  return selected
 end
 
 return select_colors()
